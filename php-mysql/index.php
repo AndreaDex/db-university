37ec3b06dde1895dab70a09b034530d991f6380a
AndreaDex <desiderio.andrea@outlook.it>
2021-07-06 16:01:08 +0200

    problem with mysqli

--- php-mysql/index.php
+++ php-mysql/index.php
@@ -5,6 +5,17 @@ define("DB_PASSWORD", "root");
 define("DB_NAME", "university");
 define("DB_PORT", "3306");
 
+
+/* if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'We don\'t have mysqli!!!';
+} else {
+    echo 'Phew we have it!';
+} */
+
+
+//var_dump(phpinfo());
+
+
 // Connessione al db tramine istanza mysqli
 $connection = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
 
@@ -12,3 +23,26 @@ $connection = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PO
 if ($connection && $connection->connect_error) {
     echo "Commection failed error: " . $connection->connect_error;
 }
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>test</h1>
+</body>
+
+</html>
